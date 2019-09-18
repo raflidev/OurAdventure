2c7ce95625797819f3013e7db653d9e2c4ffdc36
Rafli Ramadhan <rafliramadhan@gmail.com>
2019-09-18 20:29:30 +0700

    first commit

---
+++ templates/footer.php
@@ -0,0 +1,17 @@
+
+<section id='footer'>
+    <div class="m-3 p-4 border text-center">
+      Copyright 2019 &copy; Rafli Ramadhan - Bekasi <br>
+      <a  target="_blank" style="font-size: 28px" href="http://instagram.com/raflidev"><i class="fab fa-instagram"></i></a>
+      <a target="_blank" style="font-size: 28px" href="http://github.com/raflidev"><i class="fab fa-github"></i></a>
+    </div>
+</section>
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+    <script src="asset/js/bootstrap.min.js"></script>
+  </body>
+</html>
